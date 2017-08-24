76816f610f16fdbfe3b94e4af1c3febfc12fb328
georgi <georgedimmitrov@gmail.com>
2017-08-24 16:54:38 +0300

    abstractfactoryaddy

---
+++ 1.CreationalPatterns/5.AbstractFactoryPattern/abstractFactoryAddy.js
@@ -0,0 +1,8 @@
+// aims to encapsulate a group of individual factories with a common goal.
+// separates the details of implementation of a set of objects from their general usage
+
+// Uses: if a system must be independent from the way the objects it creates are generated
+// or it needs to work with multiple types of objects
+const abstractVehicleFactory = (function() {
+  
+})();
